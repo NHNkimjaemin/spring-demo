300525505e83098cd865c277501a59aee3e324ee
NHNkimjaemin <jaemin.kim@nhn.com>
2021-01-19 11:03:13 +0900

    ADD thymeleaf pj

---
+++ demoth/src/test/java/com/example/demoth/DemothApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemothApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
